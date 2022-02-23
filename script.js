261d211e25667d9c0987f4e61c9271d02fb44544
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-02-23 12:38:35 -0800

    update project

---
+++ script.js
@@ -0,0 +1,15 @@
+//draw on canvas
+const canvas = document.getElementById('canvasDraw');
+const draw = canvas.getContext("2d");
+//draw Hang Mans post
+//head
+//body
+//Left arm
+//Left leg
+//right arm
+//right leg
+//Words to solve
+const words = ["HTML", "Javascript", "CSS"];
+//game logic
+//win condtion
+// lose condition
