2cc397e5660ba4f61a8215640a31beb6b99f9316
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-02-23 16:20:38 -0800

    updated

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ const canvas = document.getElementById('canvasDraw');
 const draw = canvas.getContext("2d");
 //draw Hang Mans post
 //head
+const head = () => {
+    draw.beginPath();
+    draw.arc(300, 200, 50, 500, Math.PI * 2, true);
+    draw.moveTo(110, 75);
+
+    draw.stroke();
+}
+
+head();
 //body
+const body = () => {
+draw.beginPath();
+draw.moveTo(200, 19000)
+draw.lineTo(300, 250);
+draw.stroke();
+}
+
+body();
 //Left arm
 //Left leg
 //right arm
 //right leg
+//update the body
+
 //Words to solve
 const words = ["HTML", "Javascript", "CSS"];
 //game logic
